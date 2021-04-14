485e69c9e32e4990a63cfda508bead4285597838
DerGut <jannik.steinmann@gmx.de>
2021-04-14 16:17:47 +0200

    runner: allow to change headlessness of browser

--- loadrunner/src/main.ts
+++ loadrunner/src/main.ts
@@ -11,7 +11,7 @@ import fs from "fs/promises";
 (async () => {
     SegfaultHandler.registerHandler();
 
-    const { runID, url, accounts } = await parseArgs(process.argv);
+    const { runID, url, accounts, headless } = await parseArgs(process.argv);
 
     rootLogger.info(`Testing ${url} with ${accounts.length} classes`);
     rootLogger.info(`runID: ${runID}`);
@@ -20,7 +20,7 @@ import fs from "fs/promises";
 
     const pwLogger = newLogger("playwright");
     const browser = await chromium.launch({ 
-        headless: true, 
+        headless,
         slowMo: 200,
         args: [
             "--disable-dev-shm-usage",
@@ -29,6 +29,9 @@ import fs from "fs/promises";
         logger: {
             isEnabled: () => true,
             log: (name, severity, message, args) => {
+                if (process.env.NODE_ENV !== "production") {
+                    return;
+                }
                 if (message instanceof Error) {
                     pwLogger.error(message);
                 } else {
@@ -56,8 +59,9 @@ import fs from "fs/promises";
     rootLogger.info(`Started all ${accounts.length} users`);
 })();
 
-async function parseArgs(args: string[]): Promise<{ runID: string, url: string, accounts: Classroom[] }> {
+async function parseArgs(args: string[]): Promise<{ runID: string, url: string, accounts: Classroom[], headless: boolean }> {
     let runID: string, url: string, accounts: string;
+    let headless: boolean;
     if (args.length > 2) {
         if (args.length < 5) {
             rootLogger.error("Not enough arguments provided");
@@ -66,6 +70,11 @@ async function parseArgs(args: string[]): Promise<{ runID: string, url: string, 
         runID = args[2];
         url = args[3];
         accounts = args[4];
+        if (args.length > 5) {
+            headless = args[5] === "true";
+        } else {
+            headless = false;
+        }
     } else {
         runID = process.env.RUN_ID || "";
         url = process.env.URL || "";
@@ -74,6 +83,7 @@ async function parseArgs(args: string[]): Promise<{ runID: string, url: string, 
             rootLogger.error("Please give RUN_ID, URL and ACCOUNTS env vars");
             process.exit(1);
         }
+        headless = process.env.HEADLESS === "true";
     }
 
     if (accounts.endsWith(".json")) {
@@ -82,7 +92,7 @@ async function parseArgs(args: string[]): Promise<{ runID: string, url: string, 
     }
 
     try {
-        return { runID, url, accounts: JSON.parse(accounts) };
+        return { runID, url, accounts: JSON.parse(accounts), headless };
     } catch (e) {
         if (e instanceof SyntaxError) {
             rootLogger.error("Error parsing accounts", e);
